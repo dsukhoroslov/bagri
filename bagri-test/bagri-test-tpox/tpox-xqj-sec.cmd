3bcb051b2ab18d98f6629a2043f140d597a3b2d3
dsukhoroslov <dsukhoroslov@gmail.com>
2015-06-22 10:04:15 +0300

    script to initialize schema

--- bagri-test/bagri-test-tpox/tpox-xqj-sec.cmd
+++ bagri-test/bagri-test-tpox/tpox-xqj-sec.cmd
@@ -1,7 +1,5 @@
 @echo off
 @
-@rem This will start a console application
-@rem demonstrating the functionality of the Coherence(tm) API
 @
 setlocal
 
@@ -14,18 +12,22 @@ rem security documents  (because 83 * 251 = 20833). 4166*5 = 3472*6 = 2976*7 = 2
 rem initialize stats series
 rem "%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" com.bagri.client.tpox.StatisticsCollector %admin_addr% %schema% QueryManagement executeXQuery InsertSecurities ./stats.txt false
 
+rem register schema
+"%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" com.bagri.common.manage.MBeanInvoker %admin_addr% %login% %password% com.bagri.xdm:type=Schema,name=default,kind=ModelManagement registerSchema ../../etc/samples/tpox/security.xsd java.lang.String
+
 rem insert securities to the cache
-"%java_exec%" -server -showversion %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" net.sf.tpox.workload.core.WorkloadDriver -w queries/insSecurity-xqj.xml -tr 2604 -u 8 
+rem "%java_exec%" -server -showversion %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" net.sf.tpox.workload.core.WorkloadDriver -w queries/insSecurity-xqj.xml -tr 2604 -u 8
 
 rem get insert statistics
-"%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" com.bagri.client.tpox.StatisticsCollector %admin_addr% %schema% QueryManagement executeXQuery InsertSecurities ./stats.txt false
+rem "%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" com.bagri.client.tpox.StatisticsCollector %admin_addr% %schema% QueryManagement executeXQuery InsertSecurities ./stats.txt false
 
 rem perform queries loopig by user count
 for /l %%x in (5, 1, 10) do (
-	echo %%x
-	"%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" net.sf.tpox.workload.core.WorkloadDriver -w queries/securities-xqj.xml -u %%x 
-	"%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" com.bagri.client.tpox.StatisticsCollector %admin_addr% %schema% QueryManagement executeXQuery Users=%%x ./stats.txt false
+ 	echo %%x
+rem 	"%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" net.sf.tpox.workload.core.WorkloadDriver -w queries/securities-xqj.xml -u %%x 
+rem 	"%java_exec%" -server %java_opts% -cp "%app_home%\target\*;%app_home%\target\lib\*" com.bagri.client.tpox.StatisticsCollector %admin_addr% %schema% QueryManagement executeXQuery Users=%%x ./stats.txt false
 )
+
 
 goto exit
 
