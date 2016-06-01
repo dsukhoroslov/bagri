18fcdbde4baf0dc475b340967ecbe82b0ce7b285
dsukhoroslov <dsukhoroslov@gmail.com>
2016-06-01 18:36:27 +0300

    minor version # increased

--- bagri-tools/bagri-tools-vvmp/src/main/java/com/bagri/visualvm/manager/BagriApplicationType.java
+++ bagri-tools/bagri-tools-vvmp/src/main/java/com/bagri/visualvm/manager/BagriApplicationType.java
@@ -28,7 +28,7 @@ public class BagriApplicationType extends ApplicationType {
 
     @Override
     public String getVersion() {
-        return "0.7.1";
+        return "0.8.1";
     }
 
     @Override
