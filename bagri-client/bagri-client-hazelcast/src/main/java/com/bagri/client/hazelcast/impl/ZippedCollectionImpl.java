82f2fc509970b52dba46fc7561422b1fb60d2497
dsukhoroslov <dsukhoroslov@gmail.com>
2017-10-13 11:21:28 +0300

    fixed results compression

--- bagri-client/bagri-client-hazelcast/src/main/java/com/bagri/client/hazelcast/impl/ZippedCollectionImpl.java
+++ bagri-client/bagri-client-hazelcast/src/main/java/com/bagri/client/hazelcast/impl/ZippedCollectionImpl.java
@@ -57,30 +57,31 @@ public class ZippedCollectionImpl<T> extends FixedCollectionImpl<T> {
 		int size = in.readInt();
 		results = new ArrayList<>(size);
 		byte[] data = in.readByteArray();
-		logger.info("readData; src length: {}; entry count: {}", data.length, size);
+		//logger.info("readData; src length: {}; entry count: {}", data.length, size);
 		ByteArrayInputStream bais = new ByteArrayInputStream(data);
 		ZipInputStream zin = new ZipInputStream(new BufferedInputStream(bais)); 
 		ZipEntry entry; 
 		while ((entry = zin.getNextEntry()) != null) {
-        	logger.info("readData; entry: {}", entry);
+        	//logger.info("readData; entry: {}", entry);
 	        int count;
 			byte[] data2 = new byte[sz_buff];  
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			BufferedOutputStream tmp = new BufferedOutputStream(baos, sz_buff);
             while ((count = zin.read(data2, 0, sz_buff)) >= 0) {
-            	logger.info("readData; count: {}", count);
+            	//logger.info("readData; count: {}", count);
             	tmp.write(data2, 0, count);
             }
             tmp.flush();
             data2 = baos.toByteArray();
             length += data2.length;
-        	logger.info("readData; length: {}", length);
+        	//logger.info("readData; length: {}", length);
 			ObjectDataInput odi = ss.createObjectDataInput(data2);
 			results.add((T) odi.readObject());
-            tmp.close();				
+            tmp.close();
+            zin.closeEntry();
 		}
 		zin.close();
-		logger.info("readData; src length: {}; result length: {}", data.length, length);
+		//logger.info("readData; src length: {}; result length: {}", data.length, length);
 	}
 
 	@Override
@@ -88,7 +89,7 @@ public class ZippedCollectionImpl<T> extends FixedCollectionImpl<T> {
 		out.writeInt(results.size());
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		ZipOutputStream zout = new ZipOutputStream(new BufferedOutputStream(baos));
-		//zout.setMethod(ZipOutputStream.DEFLATED);
+		zout.setMethod(ZipOutputStream.DEFLATED);
 		int cnt = 0;
 		int length = 0;
 		for (Object result: results) {
@@ -97,12 +98,13 @@ public class ZippedCollectionImpl<T> extends FixedCollectionImpl<T> {
 			tmp.writeObject(result);
 			byte[] data = tmp.toByteArray();
 			zout.write(data, 0, data.length);
+			zout.closeEntry();
 			length += data.length;
 			cnt++;
 		}
 		zout.flush();
 		byte[] data = baos.toByteArray();
-		logger.info("writeData; src length: {}; result length: {}; entry count: {}", length, data.length, results.size());
+		//logger.info("writeData; src length: {}; result length: {}; entry count: {}", length, data.length, results.size());
 		out.writeByteArray(data);
 		zout.close();
 	}
