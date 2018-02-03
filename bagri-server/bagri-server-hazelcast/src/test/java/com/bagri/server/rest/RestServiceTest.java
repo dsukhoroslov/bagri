bd58927e3be818f3eb0d1a2194dc97e6c13e3323
dsukhoroslov <dsukhoroslov@gmail.com>
2018-02-03 13:20:43 +0300

    preparing release

--- bagri-server/bagri-server-hazelcast/src/test/java/com/bagri/server/rest/RestServiceTest.java
+++ bagri-server/bagri-server-hazelcast/src/test/java/com/bagri/server/rest/RestServiceTest.java
@@ -29,6 +29,20 @@ public abstract class RestServiceTest {
 	protected static ClassPathXmlApplicationContext ctx;
     protected static WebTarget target;
     protected static String cuid;
+    
+	//static {
+	//    //for localhost testing only
+	//    javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+	//    new javax.net.ssl.HostnameVerifier(){
+    //
+	//        public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+	//            if (hostname.equals("localhost")) {
+	//                return true;
+	//            }
+	//            return false;
+	//        }
+	//    });
+	//}    
 
 	public static void startServices() throws Exception {
 		//System.setProperty("bdb.log.level", "trace");
